384a283ba1dcd9f4a97cbb95deb4bb6ad56ed248
Rodrigo Santos <ro.augusto@gmail.com>
2021-08-16 18:32:41 -0300

    Ajustes dos fontes

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,12 +2,13 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookstoreApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 }
